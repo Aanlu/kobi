720b523c79fe66f80d0a5d64759c4421196b7fb1
Aanlu <luisantonio6641@gmail.com>
2025-11-06 13:29:07 -0700

    Se implemento el inicio de sesión por medio de google y facebook y se agrego el cambio de contraseña por medio de correo, llegando un link al correo destino.

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAycV6E9jTbPwV4qZsER3XFy1iRRdiAc94",
+    authDomain: "kobi-ee1a1.firebaseapp.com",
+    projectId: "kobi-ee1a1",
+    storageBucket: "kobi-ee1a1.firebasestorage.app",
+    messagingSenderId: "979366954149",
+    appId: "1:979366954149:web:31be274915461794d6b94c",
+    measurementId: "G-W2LMVCCV3Q"
+};
+    
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
